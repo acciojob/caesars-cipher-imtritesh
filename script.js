d6d74fe86c19a6a28330f104becc8c0130112e6f
imtritesh <103854328+imtritesh@users.noreply.github.com>
2023-03-15 15:49:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 // Your Script here.
+function rot13(str) {
+  let decodedStr = '';
+  for (let i = 0; i < str.length; i++) {
+    let charCode = str.charCodeAt(i);
+    if (charCode >= 65 && charCode <= 90) { // check if it's an uppercase letter
+      charCode = ((charCode - 65 + 13) % 26) + 65; // apply the ROT13 shift
+    }
+    decodedStr += String.fromCharCode(charCode);
+  }
+  return decodedStr;
+}
